4b15b794a939dae38f6688d28b13a08b310cf6b5
Filip Siwiec <filipsiwiec@gmal.com>
2020-07-19 12:24:28 +0200

    react: section 3 - part 13

--- App.js
+++ App.js
@@ -1,24 +1,43 @@
-class App extends React.Component{
+//Funkcja strzałkowa a deklarowanie metody. Bind
+//setState - przekazanie do metody obiektu i funkcji
+//Definiowanie state w konstruktorze
+
+class App extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     text: ""
+  //   }
+  //   this.handleClick = this.handleClick.bind(this)
+  // }
 
   state = {
-    text: "",
+    text: ""
   }
 
-  handleOnclick = () => {
-    const letter = "a";
-    this.setState({
-      text: this.state.text + letter,
+  handleClick = () => {
+    // this.state.text += "a";
+    // console.log(this.state.text);
+    // console.log(this);
+    const letter = "a"
+
+    // this.setState({
+    //   text: this.state.text + letter
+    // })
+
+    this.setState(() => ({
+      text: this.state.text + letter
     })
+    )
   }
-
-  render(){
-    return(
-      <div>
-        <button onClick={this.handleOnclick}>dodajemy A</button>
+  render() {
+    return (
+      <React.Fragment>
+        <button onClick={this.handleClick}>Dodaj "A"</button>
         <h1>{this.state.text}</h1>
-      </div>
+      </React.Fragment>
     )
   }
 }
 
-ReactDOM.render(<App/>, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById("root"))
