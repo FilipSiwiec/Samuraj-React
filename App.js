d4fc56c550508bb74751973c1b7cb382ca979536
Filip Siwiec <filipsiwiec@gmal.com>
2020-08-25 20:33:16 +0200

    section 3 - part 11

--- App.js
+++ App.js
@@ -1,34 +1,43 @@
-const Header = () => {
-  return(
-    <div>
-      <h1>Witaj na stronie</h1>
-    </div>
-  )
+class ShoppingList extends React.Component {
+
+  state = {
+    item1: "ogorki",
+    item2: "pietruszka",
+    item3: "mleko"
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>To jest moja lista zakupow</h1>
+        <ul>
+          <ItemList name={this.state.item1} example="2+2" />
+          <ItemList name={this.state.item2} />
+          <ItemList name={this.state.item3} />
+        </ul>
+      </div>
+    )
+  }
+
 }
 
-// komponent klasowy
 
-class Blog extends React.Component{
-  // state={
-  //   number: 0,
-  // }
-  render(){
-    return(
-      <section>
-        <h2>Artykul</h2>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam, commodi? Consequatur, delectus assumenda. Quis, consequatur tenetur, alias quasi reprehenderit iure labore vero accusamus voluptates, placeat soluta impedit minima fuga natus?</p>
-      </section>
+class ItemList extends React.Component {
+  render() {
+    return (
+      <div>
+        {this.props.name}, {this.props.example}
+      </div>
     )
   }
 }
 
-const App = () => {
-  return (
-    <div>
-    <Header/>
-    <Blog/>
-    </div>
-  )
-}
+// const ItemList = (props) => {
+//   return (
+//     <div>
+//       {props.name}, {props.example}
+//     </div>
+//   )
+// }
 
-ReactDOM.render(<App/>, document.getElementById("root"));
+ReactDOM.render(<ShoppingList />, document.getElementById("root"));
