24472d02576e09a6b090291c2caf3a7b5c523c33
Filip Siwiec <filipsiwiec@gmal.com>
2020-08-25 15:05:03 +0200

    section 3 - part 10

--- App.js
+++ App.js
@@ -1,25 +1,34 @@
-const header = <h1 className="title">Witaj na stronie!</h1>
+const Header = () => {
+  return(
+    <div>
+      <h1>Witaj na stronie</h1>
+    </div>
+  )
+}
 
-const classBig = "big";
+// komponent klasowy
 
-const handleClick = () => alert("klik!")
-const main = (
-  <div>
-    <h1 person="osoby" onClick={handleClick} className="red">Pierwszy artykuł</h1>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae iste similique itaque a sequi provident corporis fuga, neque libero dolore quidem iure aliquam mollitia, velit pariatur quos sint iusto culpa?</p>
-  </div>
-)
+class Blog extends React.Component{
+  // state={
+  //   number: 0,
+  // }
+  render(){
+    return(
+      <section>
+        <h2>Artykul</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam, commodi? Consequatur, delectus assumenda. Quis, consequatur tenetur, alias quasi reprehenderit iure labore vero accusamus voluptates, placeat soluta impedit minima fuga natus?</p>
+      </section>
+    )
+  }
+}
 
-const text = "stopkaaa";
-const largeTxt = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore temporibus quasi repudiandae, quia beatae, quas et nulla, soluta reprehenderit consequuntur voluptatum a delectus fuga? Minus, unde? Iusto fugit non quisquam."
-const footer = (
-  <footer>
-    {largeTxt}
-    <p className={classBig}>{text}</p>
+const App = () => {
+  return (
+    <div>
+    <Header/>
+    <Blog/>
+    </div>
+  )
+}
 
-  </footer>
-)
-
-const app = [header, main, footer]
-
-ReactDOM.render(app, document.getElementById('root'))
+ReactDOM.render(<App/>, document.getElementById("root"));
