c446253ab7fd40f6e7273f1dc2d1b1d8c0ff5a20
Filip Siwiec <filipsiwiec@gmal.com>
2020-08-31 22:24:42 +0200

    section 5 - project 2 - part 2

--- App.js
+++ App.js
@@ -1,30 +1,38 @@
 class App extends React.Component {
     state = {
-      availableProducts: 7,
-      shoppingCart: 1,
+        availableProducts: 7,
+        shoppingCart: 1,
     }
-  
+
     handleRemoveFromCart = () => {
-      this.setState({
-        shoppingCart: this.state.shoppingCart - 1,
-      })
+        this.setState({
+            shoppingCart: this.state.shoppingCart - 1,
+        })
     }
-  
+
     handleAddToCart = () => {
-      this.setState({
-        shoppingCart: this.state.shoppingCart + 1,
-      })
+        this.setState({
+            shoppingCart: this.state.shoppingCart + 1,
+        })
     }
-  
+
+    buyBtn = () => {
+        this.setState({
+            shoppingCart: this.state.availableProducts - this.state.shoppingCart,
+            shoppingCart: 0
+        })
+    }
+
     render() {
-      return (
-        <div>
-          <button disabled={this.state.shoppingCart ? false : true} onClick={this.handleRemoveFromCart}>-</button>
-          <span> {this.state.shoppingCart} </span>
-          <button disabled={this.state.shoppingCart === this.state.availableProducts ? true : false} onClick={this.handleAddToCart}>+</button>
-        </div>
-      )
+        return (
+            <div>
+                <button disabled={this.state.shoppingCart ? false : true} onClick={this.handleRemoveFromCart}>-</button>
+                <span> {this.state.shoppingCart} </span>
+                <button disabled={this.state.shoppingCart === this.state.availableProducts ? true : false} onClick={this.handleAddToCart}>+</button>
+                {this.state.shoppingCart > 0 && <button onClick={this.buyBtn}>Kup!</button>}
+            </div>
+        )
     }
-  }
-  
-  ReactDOM.render(<App />, document.getElementById('root'))
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
