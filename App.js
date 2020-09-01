d056c49379124f9079fa1173491bea3cd51e7a86
Filip Siwiec <filipsiwiec@gmal.com>
2020-09-01 12:21:18 +0200

    section 5 - project 3 - part 1

--- App.js
+++ App.js
@@ -1,49 +1,15 @@
-
-// Wyszarzenie liczby 0 - css
-// destrukturyzacja
-
 class App extends React.Component {
     state = {
-      availableProducts: 7,
-      shoppingCart: 1,
+        items: ["gruszka", "jabłko", "pomidory"],
     }
-  
-    handleRemoveFromCart = () => {
-      this.setState({
-        shoppingCart: this.state.shoppingCart - 1,
-      })
-    }
-  
-    handleAddToCart = () => {
-      this.setState({
-        shoppingCart: this.state.shoppingCart + 1,
-      })
-    }
-  
-    handleBuy = () => {
-      this.setState({
-        availableProducts: this.state.availableProducts - this.state.shoppingCart,
-        shoppingCart: 0,
-      })
-    }
-  
+
     render() {
-        const {shoppingCart,availableProducts} = this.state
-  
-      const style = shoppingCart === 0 ? { opacity: 0.3 } : null;
-  
-      return (
-        <div>
-          <button disabled={shoppingCart ? false : true} onClick={this.handleRemoveFromCart}>-</button>
-  
-          <span style={style}> {shoppingCart} </span>
-  
-          <button disabled={shoppingCart === availableProducts ? true : false} onClick={this.handleAddToCart}>+</button>
-          {shoppingCart > 0 && <button onClick={this.handleBuy}>Kup</button>}
-  
-        </div>
-      )
+        return (
+            <ul>
+                {this.state.items.map((item) => { return <li key={item}>{`owocem jest ${item}`}</li> })}
+            </ul>
+        )
     }
-  }
-  
-  ReactDOM.render(<App />, document.getElementById('root'))
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
