afad298f05f7f6b9fd063359ceab156f8e7f1db6
Filip Siwiec <filipsiwiec@gmal.com>
2020-09-01 18:39:59 +0200

    section 5 - project 3 - part 4

--- App.js
+++ App.js
@@ -1,15 +1,56 @@
-class App extends React.Component {
+const data = {
+    users: [
+      {
+        id: 1,
+        age: 29,
+        name: "Arek",
+        sex: "male",
+      },
+      {
+        id: 2,
+        age: 49,
+        name: "Marta",
+        sex: "female",
+      },
+      {
+        id: 3,
+        age: 19,
+        name: "Stasia",
+        sex: "female",
+      },
+      {
+        id: 4,
+        age: 24,
+        name: "Karol",
+        sex: "male",
+      }
+    ]
+  }
+  
+  const Item = ({ user }) => (
+    <div className="userInfo">
+      <h1>{user.name}</h1>
+      <p>Informacje o użytkowniku</p>
+      <p>Wiek użytkownika: <strong>{user.age}</strong></p>
+      <p>Płeć użytkownika: {user.sex}</p>
+    </div>
+  )
+  class ListItems extends React.Component {
     state = {
-        items: ["gruszka", "jabłko", "pomidory"],
+  
     }
-
     render() {
-        return (
-            <ul>
-                {this.state.items.map((item) => { return <li key={item}>{`owocem jest ${item}`}</li> })}
-            </ul>
-        )
+      let users = this.props.data.users
+      users = users.filter((user) => user.sex === "male")
+      const Items = users.map(user => <Item key={user.id} user={user} />)
+  
+      return (
+        <ul>
+          {Items}
+        </ul>
+      )
     }
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
+  }
+  
+  ReactDOM.render(<ListItems data={data} />, document.getElementById('root'))
+  
