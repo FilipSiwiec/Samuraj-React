ff957c941544c8427f69849d8a45b5a07d968563
Filip Siwiec <filipsiwiec@gmal.com>
2020-09-02 14:51:52 +0200

    section 5 - project 3 - part 5

--- App.js
+++ App.js
@@ -1,56 +1,79 @@
 const data = {
     users: [
-      {
-        id: 1,
-        age: 29,
-        name: "Arek",
-        sex: "male",
-      },
-      {
-        id: 2,
-        age: 49,
-        name: "Marta",
-        sex: "female",
-      },
-      {
-        id: 3,
-        age: 19,
-        name: "Stasia",
-        sex: "female",
-      },
-      {
-        id: 4,
-        age: 24,
-        name: "Karol",
-        sex: "male",
-      }
+        {
+            id: 1,
+            age: 29,
+            name: "Arek",
+            sex: "male",
+        },
+        {
+            id: 2,
+            age: 49,
+            name: "Marta",
+            sex: "female",
+        },
+        {
+            id: 3,
+            age: 19,
+            name: "Stasia",
+            sex: "female",
+        },
+        {
+            id: 4,
+            age: 24,
+            name: "Karol",
+            sex: "male",
+        }
     ]
-  }
-  
-  const Item = ({ user }) => (
-    <div className="userInfo">
-      <h1>{user.name}</h1>
-      <p>Informacje o użytkowniku</p>
-      <p>Wiek użytkownika: <strong>{user.age}</strong></p>
-      <p>Płeć użytkownika: {user.sex}</p>
-    </div>
-  )
-  class ListItems extends React.Component {
+}
+
+const Items = (props) => {
+    return (
+        <div>
+            <h1>{props.user.name}</h1>
+            <p>Informacje o uzytkowniku</p>
+            <p>twoj wiek to: {props.user.age}</p>
+            <p>twoja plec to: {props.user.sex}</p>
+        </div>
+    )
+}
+
+class App extends React.Component {
+
     state = {
-  
+        select: "all"
     }
+
+    userList = () => {
+        let users = this.props.data.users
+        switch (this.state.select) {
+            case "all":
+                return users.map(user => <Items user={user} />)
+            case "female":
+                users = users.filter(user => user.sex === "female");
+                return users.map(user => <Items user={user} />)
+            case "male":
+                users = users.filter(user => user.sex === "male");
+                return users.map(user => <Items user={user} />)
+        }
+    }
+
+    handleChange = (option) => {
+        this.setState({
+            select: option
+        })
+    }
+
     render() {
-      let users = this.props.data.users
-      users = users.filter((user) => user.sex === "male")
-      const Items = users.map(user => <Item key={user.id} user={user} />)
-  
-      return (
-        <ul>
-          {Items}
-        </ul>
-      )
+        return (
+            <div>
+                <button onClick={this.handleChange.bind(this, "all")}>Wszyscy</button>
+                <button onClick={this.handleChange.bind(this, "female")}>Kobiety</button>
+                <button onClick={this.handleChange.bind(this, "male")}>Mezczyzni</button>
+                {this.userList()}
+            </div>
+        )
     }
-  }
-  
-  ReactDOM.render(<ListItems data={data} />, document.getElementById('root'))
-  
+}
+
+ReactDOM.render(<App data={data} />, document.getElementById("root"))
