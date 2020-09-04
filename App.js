e7289967703914e9d642a6374a36ff07cc855c43
Filip Siwiec <filipsiwiec@gmal.com>
2020-09-04 14:42:57 +0200

    section 5 - task 5 finished

--- App.js
+++ App.js
@@ -1,67 +1,56 @@
-class App extends React.Component {
-    state = { 
-        city: "London",
+class Form extends React.Component {
+    state = {
+        city: "Londyn",
         text: "",
-        check: false,
-        select: 2
-     }
-
-     cityChange = (e) => {
-         this.setState({
-             city: e.target.value
-         })
-     }
-
-     textChange = (e) => {
-        this.setState({
-            text: e.target.value
-        })
+        isLoved: true,
+        number: "2",
     }
 
-    boxChange = (e) => {
-        this.setState({
-            check: e.target.checked
-        })
+    handleOnClick = (e) => {
+        if (e.target.type === "checkbox"){
+            this.setState({
+                [e.target.name]: e.target.checked 
+            })
+        }else{
+            this.setState({
+                [e.target.name]: e.target.value
+            })
+        }
     }
+   
 
-    selectChange = (e) => {
-        this.setState({
-            select: e.target.value
-        })
-    }
+    render() {
+        return (
+            <div>
+                <label>
+                    Podaj miasto
+            <input name="city" value={this.state.city} onChange={this.handleOnClick} type="text" />
+                </label>
+                <br />
+                <label>Napisz coś o tym mieście
+            <textarea name="text" value={this.state.text} onChange={this.handleOnClick}></textarea>
+                </label>
+                <br />
+                <label>
+                    Czy lubisz to miasto?
+            <input name="isLoved" type="checkbox" checked={this.state.isLoved} onChange={this.handleOnClick} />
+                </label>
+                <br />
+                <label>
+                    Ile razy byliście w tym mieście?
+            <select name="number" value={this.state.number} onChange={this.handleOnClick}>
+                        <option value="0">0</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="more">więcej</option>
+                    </select>
+                </label>
+            </div>
 
-    render() { 
-        return ( 
-        <form>
-            <label>
-                Podaj miasto:
-                <input value={this.state.city} onChange={this.cityChange} type="text"/>
-            </label>
-            <br/>
-            <label>
-                Napisz cos o tym miescie:
-                <textarea value={this.state.text} onChange={this.textChange}></textarea>
-            </label>
-            <br/>
-            <label>
-                Czy lubisz to miasto?
-                <input type="checkbox" onChange={this.boxChange} checked={this.state.check}/>
-            </label>
-            <br/>
-            <label>
-                Ile razy byles w tym miescie?
-                <select value={this.state.select} onChange={this.selectChange}> 
-                    <option value="0">0</option>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="many">wiele</option>
-                </select>
-            </label>
-        </form>
-         );
+        )
     }
 }
- 
-ReactDOM.render(<App/>, document.getElementById("root"))
+
+
+ReactDOM.render(<Form />, document.getElementById('root'))
