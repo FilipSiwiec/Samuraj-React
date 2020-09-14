7d0b2c54e51f1e2c03f4ee2b806e32013b1fe04c
Filip Siwiec <filipsiwiec@gmal.com>
2020-09-14 15:46:00 +0200

    section 6 - clock with buton

--- App.js
+++ App.js
@@ -1,110 +1,70 @@
-const Cash = (props) => {
-    const value = (props.cash / props.ratio * props.price).toFixed(2)
+class App extends React.Component {
+  state = {
+    active: true
+  }
+
+  handleClick = () => {
+    this.setState(state => ({
+      active: !state.active
+    }))
+  }
+
+  render() {
     return (
-      <div>{props.title} {props.cash <= 0 ? "" : value}</div>
+      <div>
+        <Switch
+          click={this.handleClick}
+          active={this.state.active}
+        />
+        {this.state.active && <Clock />}
+      </div>
     )
   }
-  
-  class ExchangeCounter extends React.Component {
-  
-    state = {
-      amount: "",
-      product: "gas"
-    }
-  
-    static defaultProps = {
-      currencies: [
-        {
-          id: 0,
-          name: 'zloty',
-          ratio: 1,
-          title: 'Wartość w złotówkach:'
-        },
-        {
-          id: 1,
-          name: 'dollar',
-          ratio: 3.6,
-          title: 'Wartość w dolarach:'
-        },
-        {
-          id: 2,
-          name: 'euro',
-          ratio: 4.1,
-          title: 'Wartość w euro:'
-        },
-        {
-          id: 3,
-          name: 'pound',
-          ratio: 4.55,
-          title: 'Wartość w funtach:'
-        },
-      ],
-      prices: {
-        electricity: .51,
-        gas: 4.76,
-        oranges: 3.79,
-      }
-    }
-  
-  
-    handleChange = e => {
-      this.setState({
-        amount: e.target.value
-      })
-    }
-  
-    handleSelect = e => {
-      this.setState({
-        product: e.target.value,
-        amount: "",
-      })
-    }
-  
-    insertSuffix(select) {
-      if (select === "electricity") return <em>kWh</em>
-      else if (select === "gas") return <em>litrów</em>
-      else if (select === "oranges") return <em>kilogramów</em>
-      else return null
-    }
-  
-    selectPrice(select) {
-      const price = this.props.prices[select]
-      return price
-    }
-  
-    render() {
-  
-      const { amount, product } = this.state;
-      const price = this.selectPrice(product)
-  
-      const calculators = this.props.currencies.map(currency => (
-        <Cash key={currency.id} ratio={currency.ratio} title={currency.title} cash={amount} price={price} />
-      ))
-  
-      return (
-        <div className="app">
-          <label>Wybierz produkt:
-          <select value={product} onChange={this.handleSelect}>
-              <option value="electricity">prąd</option>
-              <option value="gas">benzyna</option>
-              <option value="oranges">pomarańcze</option>
-            </select>
-  
-          </label>
-          <br />
-          <label>
-            <input
-              type="number"
-              value={this.state.amount}
-              onChange={this.handleChange}
-            />
-            {this.insertSuffix(this.state.product)}
-          </label>
-          {calculators}
-        </div>
-      )
-  
-    }
+}
+
+const Switch = (props) => {
+  return (
+    <button onClick={props.click}>{props.active ? "wylacz" : "wlacz"}</button>
+  )
+}
+
+class Clock extends React.Component {
+  state = {
+    time: this.getTime()
   }
-  ReactDOM.render(<ExchangeCounter />, document.getElementById('root'))
-  
+
+  getTime() {
+    const time = new Date()
+    return ({
+      hours: time.getHours(),
+      minutes: time.getMinutes(),
+      seconds: time.getSeconds()
+    })
+  }
+
+  setTime = () => {
+    const time = this.getTime()
+    this.setState({
+      time
+    })
+  }
+
+  componentDidMount() {
+    this.interal = setInterval(this.setTime, 1000)
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.interal)
+  }
+
+  render() {
+    const { hours, minutes, seconds } = this.state.time
+    return (
+      <div>
+        {hours} : {minutes > 9 ? minutes : `0${minutes}`} : {seconds}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"))
